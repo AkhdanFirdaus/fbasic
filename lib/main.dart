d2e164b60f69dd2610542be7dd288ccddfbc042f
Akhdan Firdaus <akhdan.musyaffa.firdaus@gmail.com>
2019-12-27 21:12:00 +0700

    Finish simpel magic ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MagicBall());
@@ -5,6 +7,44 @@ void main() => runApp(MagicBall());
 class MagicBall extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Ask Me Anything'),
+        ),
+        body: Container(
+          child: Ball(),
+        ),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int numBall = 1;
+  randomImg() {
+    print('Terklik gess!');
+    setState(() {
+      numBall = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$numBall.png'),
+        onPressed: () => randomImg(),
+      ),
+    );
   }
 }
