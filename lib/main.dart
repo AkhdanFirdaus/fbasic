49bcd14fda7e2f858ee79e3c2577db1e6c38fef0
Akhdan Firdaus <akhdan.musyaffa.firdaus@gmail.com>
2019-12-27 21:19:08 +0700

    Initial Xylophone assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MagicBall());
+void main() => runApp(Xylophone());
 
-class MagicBall extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Text('Ask Me Anything'),
-        ),
-        body: Container(
-          child: Ball(),
-        ),
-      ),
-    );
-  }
-}
-
-class Ball extends StatefulWidget {
-  Ball({Key key}) : super(key: key);
-
-  @override
-  _BallState createState() => _BallState();
-}
-
-class _BallState extends State<Ball> {
-  int numBall = 1;
-  randomImg() {
-    print('Terklik gess!');
-    setState(() {
-      numBall = Random().nextInt(5) + 1;
-    });
-  }
+class Xylophone extends StatelessWidget {
+  const Xylophone({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-        child: Image.asset('images/ball$numBall.png'),
-        onPressed: () => randomImg(),
-      ),
-    );
+    return Container();
   }
 }
