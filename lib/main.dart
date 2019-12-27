75d155d11058526d2df05924c321144c6cec1496
Akhdan Firdaus <akhdan.musyaffa.firdaus@gmail.com>
2019-12-27 20:27:49 +0700

    Initial ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,10 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() => runApp(Dice());
+void main() => runApp(MagicBall());
 
-class Dice extends StatelessWidget {
+class MagicBall extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    );
-  }
-}
-
-class DicePage extends StatefulWidget {
-  DicePage({Key key}) : super(key: key);
-
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 2;
-  void changeDiceFace() {
-    setState(() {
-      rightDiceNumber = Random().nextInt(6) + 1;
-      leftDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () => changeDiceFace(),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-              onPressed: () => changeDiceFace(),
-            ),
-          ),
-        ],
-      ),
-    );
+    return MaterialApp();
   }
 }
